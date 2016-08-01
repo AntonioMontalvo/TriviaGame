f666ad06d3fdbece10791b27068417e04ddfd04e
Antonio Montalvo <montalvocode@yahoo.com>
2016-07-31 18:07:23 -0700

    add timer object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,27 @@
-console.log('hello');
+window.onload = function(){
+    $('#display-time').html(timer.time );
+};
+
+var counter;
+
+var timer = {
+  time: 30,
+  startTime: function(){
+  counter = setInterval(timer.decrement, 1000);
+  },
+  decrement: function(){
+    timer.time--;
+    $('#display-time').html(timer.time );
+      if (timer.time === 0){
+        timer.reset();
+      }
+  },
+  reset: function(){
+    timer.time = 30;
+  },
+  stop: function (){
+    clearInterval(counter);
+  }
+}
+
+timer.startTime();
